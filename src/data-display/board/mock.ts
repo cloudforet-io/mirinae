31fdbe68f66cddb82bf1b40d7b5c6b910493cc19
Yongtae Park <samuel.park@mz.co.kr>
2022-11-16 15:32:02 +0900

    feat(board): create `PBoard` component

---
+++ src/data-display/board/mock.ts
@@ -0,0 +1,64 @@
+import { standardIconActionSet } from '@/data-display/board-item/mock';
+import type { BoardItemProps } from '@/data-display/board-item/type';
+
+export const basicItemSets: BoardItemProps[] = [
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        title: 'title',
+        description1: 'description1',
+        description2: 'description2',
+        iconButtonSets: standardIconActionSet,
+    },
+];
+
+export const boardStandardItemSets: BoardItemProps[] = [
+    {
+        leftIcon: 'admin',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        leftIcon: 'aws-network',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        leftIcon: 'aws-ec2',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        leftIcon: 'ic_collector_tags',
+        iconButtonSets: standardIconActionSet,
+    },
+    {
+        leftIcon: 'ic_server',
+        iconButtonSets: standardIconActionSet,
+    },
+];
