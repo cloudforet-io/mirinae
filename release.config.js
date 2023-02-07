f5ee084853c21f95878f5c11fd3cb6a0d815e01e
Wanjin Noh <wanzargen@gmail.com>
2023-02-07 15:52:49 +0900

    ci(workflow): add branch to release workflow

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
     branches: ['master',
         { name: '1.1.x', range: '1.1.x', channel: '1.1.x' },
         { name: 'beta', prerelease: true, channel: 'beta' },
-        { name: '^(console-release-)+([0-9])?(\\.+([0-9]))?(\\.+([0-9]))$', prerelease: true, channel: 'console-release' },
+        { name: '(console-release-)([0-9]+)?(.[0-9]+)?(.[0-9]+)', prerelease: true, channel: 'console-release' },
     ],
     plugins: [
         '@semantic-release/commit-analyzer',
