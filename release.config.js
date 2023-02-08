17f7a9ba00752ab3b190b7fc621046a39fc2f653
Yongtae Park <samuel.park@mz.co.kr>
2023-02-08 17:47:02 +0900

    fix: update release.config.js and release workflow

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-    branches: ['master',
-        { name: '1.1.x', range: '1.1.x', channel: '1.1.x' },
-        { name: 'beta', prerelease: true, channel: 'beta' }],
+const config = {
+    branches: [
+        'master',
+        { name: 'beta', prerelease: true, channel: 'beta' },
+        { name: 'v([0-9]+)(.[0-9]+)(.[0-9]+)-beta(.[0-9]+)', prerelease: true },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
@@ -11,3 +13,5 @@ module.exports = {
         '@semantic-release/github',
     ],
 };
+
+module.exports = config;
